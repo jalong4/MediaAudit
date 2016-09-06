0e0f34790067c2d873f7e533367916cc45cd0431
Jim Long <jim.long@verizon.com>
2016-09-05 17:24:23 -0700

    Initial Commit

---
+++ MediaAudit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MediaAudit
+//
+//  Created by Jim Long on 9/5/16.
+//  Copyright © 2016 Jim Long. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ MediaAudit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MediaAudit
+//
+//  Created by Jim Long on 9/5/16.
+//  Copyright © 2016 Jim Long. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
